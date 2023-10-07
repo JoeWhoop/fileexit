36d4399818af7345e019f7cd03ba49d333454090
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2023-10-07 19:04:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-// JavaScript to handle button click and redirection
+//Javascript
 document.getElementById("goButton").addEventListener("click", function () {
     var selectedOption = document.getElementById("optionDropdown").value;
 
     if (selectedOption === "Lukasz Chojecki") {
-        window.location.href = "https://web.miniextensions.com/Bwk6PtCv1a4zbRONadUU?prefill_Mechanic=ŁUKASZ_CHOJECKI&prefill_Station=D3"; // Corrected URL
+        window.location.href = "https://web.miniextensions.com/Bwk6PtCv1a4zbRONadUU?prefill_Mechanic=ŁUKASZ_CHOJECKI&prefill_Station=D3"; // 
     } else if (selectedOption === "Aleks Czarnecki") {
-        window.location.href = "https://google.com"; // Redirect to Google for Aleks Czarnecki
+        window.location.href = "https://google.com"; // Redirect dla Aleks Czarnecki
     } else if (selectedOption === "Kacper Wydra") {
-        window.location.href = "https://youtube.com"; // Redirect to YouTube for Kacper Wydra
+        window.location.href = "https://youtube.com"; // Redirect dla Kacper Wydra
    } else if (selectedOption === "Sebastian Widuliński") {
-        window.location.href = "https://google.com"; // Redirect to Google for Sebastian Widuliński
+        window.location.href = "https://google.com"; // Redirect Dla Sebastian Widuliński
     } else if (selectedOption === "Mateusz Gryglicki") {
-        window.location.href = "https://youtube.com"; // Redirect to YouTube for Mateusz Gryglicki
+        window.location.href = "https://youtube.com"; // Redirect dla Mateusz Gryglicki
    } else if (selectedOption === "Mateusz Wiatr") {
-        window.location.href = "https://google.com"; // Redirect to Google for Mateusz Wiatr
+        window.location.href = "https://google.com"; // Redirect  dla Mateusz Wiatr
     } else {
         alert("Please select an option before clicking Go.");
     }
