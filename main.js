07f87b3a3df906eff30ce94a711e222debfe434b
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2023-10-07 18:23:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ document.getElementById("goButton").addEventListener("click", function () {
 
     if (selectedOption === "piotr") {
         window.location.href = "https://example.com/piotr"; // Replace with the desired URL for Piotr
-    } else if (selectedOption === "krzysztof") {
+    } else if (selectedOption === "Lukasz Chojecki) {
         window.location.href = "https://web.miniextensions.com/Bwk6PtCv1a4zbRONadUU?prefill_Mechanic=ŁUKASZ_CHOJECKI&prefill_Station=D3"; // Updated URL for Krzysztof
-    } else if (selectedOption === "dzik") {
+    } else if (selectedOption === "Aleks Czarnecki") {
         window.location.href = "https://google.com"; // Redirect to Google for Dzik
-    } else if (selectedOption === "joe") {
+    } else if (selectedOption === "Kacper Wydra ") {
         window.location.href = "https://youtube.com"; // Redirect to YouTube for Joe
+   } else if (selectedOption === "Sebastian Widuliński") {
+        window.location.href = "https://google.com"; // Redirect to Google for Dzik
+    } else if (selectedOption === "Mateusz Gryglicki") {
+        window.location.href = "https://youtube.com"; // Redirect to YouTube for Joe
+   } else if (selectedOption === "Mateusz Wiatr") {
+        window.location.href = "https://google.com"; // Redirect to Google for Dzik
     } else {
         alert("Please select an option before clicking Go.");
     }
